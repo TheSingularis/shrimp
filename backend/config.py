e98afb1abfa1ae58bc050e9fa8c15ab346d9495f
Jordan Sluiter <jordan.sluiter@icloud.com>
2026-03-17 16:03:54 -0500

    reset

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+WATCHED_DIRS: dict[str, str] = {
+    # "code":     "/home/youruser/code",
+    # "obsidian": "/home/youruser/obsidian/vault",
+}
+
+OLLAMA_MODEL = "qwen2.5-coder:7b"
+EMBED_MODEL  = "nomic-embed-text"
+CHROMA_PATH  = "./chroma_db"
+OLLAMA_BASE_URL = "http://localhost:11434"
