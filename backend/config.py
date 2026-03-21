3a1d345bd8ad74837ffd08c71c655699b9945458
Jordan Sluiter <jordan.sluiter@icloud.com>
2026-03-21 17:06:02 -0500

    chore: changed config for testing

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 OLLAMA_HOST = "http://127.0.0.1:11434"
-OLLAMA_MODEL = "qwen2.5-coder:7b"
+OLLAMA_MODEL = "deepseek-r1:14b"
 EMBED_MODEL = "nomic-embed-text"
 CHROMA_PATH = "./chroma_db"
 
-WATCHED_DIRS: list[dict] = [{'name': 'code', 'path': '~/Documents/Code Projects/test', 'enabled': True}, {'name': 'obsidian', 'path': '~/Documents/Personal/LLM Test', 'enabled': True}, {'name': 'shrimp', 'path': '~/Documents/Code Projects/shrimp', 'enabled': True}]
+WATCHED_DIRS: list[dict] = [{'name': 'code', 'path': '~/Documents/Code Projects/test', 'enabled': False}, {'name': 'obsidian', 'path': '~/Documents/Personal/LLM Test', 'enabled': True}, {'name': 'shrimp', 'path': '~/Documents/Code Projects/shrimp', 'enabled': True}]
